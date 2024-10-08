da1474113d0694be5e20e83e231b7ab8b4076b63
Soukarsha Dhar <soukarshadhar91@gmail.com>
2024-10-08 12:36:42 +0530

    Removed unwanted code

--- Array/IsArray/index.js
+++ Array/IsArray/index.js
@@ -3,13 +3,3 @@ Array.myIsArray = function (arg) {
 };
 
 console.log(Array.myIsArray([]));
-
-function A() {
-  this.type = "Function constructor";
-}
-A.prototype.getType = function () {
-  console.log(this);
-};
-
-const C = new A();
-console.log(C);
